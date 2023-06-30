efcd9125344ad8dc2580588e8721b81332bbf437
henry merrilees <henry@merrile.es>
2023-06-30 23:48:20 +0000

    setup, still broken

--- setup.sh
+++ setup.sh
@@ -1,16 +1,10 @@
 mkdir scripts
 cd scripts
 
-# install conda
-wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh
-chmod +x Anaconda3-2023.03-1-Linux-x86_64.sh
-./Anaconda3-2023.03-1-Linux-x86_64.sh
+# install micromamba
+curl micro.mamba.pm/install.sh | bash
 cd ..
 source ~/.bashrc # hack to activate conda
-
-# install colbert
-git clone https://github.com/stanford-futuredata/ColBERT
-cd ColBERT
 
 #install git-lfs
 sudo apt-get install git-lfs
@@ -19,9 +13,34 @@ git lfs install
 # install lotte
 mkdir downloads
 cd downloads
-git clone https://huggingface.co/datasets/colbertv2/lotte
 git clone https://huggingface.co/datasets/colbertv2/lotte_passages
-git clone https://huggingface.co/colbert-ir/colbertv2.0
+
+
+micromamba -y env create -f conda_env.yml
+micromamba activate colbert
+
+
+
+
+
+
+
+
+# install conda
+wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh
+chmod +x Anaconda3-2023.03-1-Linux-x86_64.sh
+./Anaconda3-2023.03-1-Linux-x86_64.sh
+cd ..
+source ~/.bashrc # hack to activate conda
+
+#install git-lfs
+sudo apt-get install git-lfs
+git lfs install
+
+# install lotte
+mkdir downloads
+cd downloads
+git clone https://huggingface.co/datasets/colbertv2/lotte_passages
 
 cd ..
 
